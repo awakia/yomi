07ebfef33eca6729ae6494ff2752a3d51ec962ce
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-05-10 15:14:27 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yomi::Application.config.secret_token = 'eb0a22f4b307cd18cca86964d534c1ae39ba2f46837cb4be656a979e893031504268729a03001624aca719f9506b2ada9bd5a82da14d6974fbf46f7303ea5786'
